799c4ef8daddc7d1b14f37c70b408d302ae886ce
AnthonyDas <anthony.das@gmail.com>
2018-11-17 12:10:14 +0000

    C++14 switch on.

--- ProjectEuler/ProjectEuler - Laptop.bat
+++ ProjectEuler/ProjectEuler - Laptop.bat
@@ -2,7 +2,7 @@ C:
 
 cd C:\Users\AnthonyDas\Documents\GitHub\ProjectEuler\ProjectEuler
 
-g++ -Wall -Wconversion -std=c++11 -mtune=native -O3 -I"C:\Users\AnthonyDas\Documents\GitHub\MathsHelper" -I"C:\Users\AnthonyDas\Documents\GitHub\Common" ProjectEuler.cpp -o ProjectEuler.exe
+g++ -Wall -Wconversion -std=c++14 -mtune=native -O3 -I"C:\Users\AnthonyDas\Documents\GitHub\MathsHelper" -I"C:\Users\AnthonyDas\Documents\GitHub\Common" ProjectEuler.cpp -o ProjectEuler.exe
 
 
 pause
